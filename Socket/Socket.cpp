62ff9e82b037e4bcd045c9a47d8d7ba9063af8eb
Kyle Gonzalez <kylegonzalez2255@gmail.com>
2018-02-19 19:24:53 +1100

    Cleaned up Scan function

--- Socket/Socket.cpp
+++ Socket/Socket.cpp
@@ -179,11 +179,9 @@ std::vector<std::pair<std::string, std::string> > Socket::scanDevices()
 	std::pair<std::string, std::string> tempPair;
 
 	inquiry_info *ii = NULL;
-    int max_rsp, num_rsp;
-    int dev_id, tempSocket, len, flags;
-    int i;
-    char addr[19] = { 0 };
-    char name[248] = { 0 };
+    int max_rsp = 255, num_rsp, dev_id, tempSocket, len = 8, flags;
+    char deviceAddress[19];
+    char deviceName[248];
 
     dev_id = hci_get_route(NULL);
     tempSocket = hci_open_dev( dev_id );
@@ -192,25 +190,25 @@ std::vector<std::pair<std::string, std::string> > Socket::scanDevices()
         perror("opening socket");
     }
 
-    len  = 8;
-    max_rsp = 255;
     flags = IREQ_CACHE_FLUSH;
     ii = new inquiry_info[max_rsp * sizeof(inquiry_info)];
-    // ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
     
     num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
-    if( num_rsp < 0 ) perror("hci_inquiry");
-
-    for (i = 0; i < num_rsp; i++) 
+    if( num_rsp < 0 )
     {
-        ba2str( &(ii + i)->bdaddr, addr);
-        memset(name, 0, sizeof(name));
-        if (hci_read_remote_name(tempSocket, &(ii+i)->bdaddr, sizeof(name), name, 0) < 0)
+    	perror("hci_inquiry");
+    }
+
+    for (int i = 0; i < num_rsp; i++) 
+    {
+        ba2str( &(ii + i)->bdaddr, deviceAddress);
+        bzero(&deviceName, sizeof(deviceName));
+        if (hci_read_remote_name(tempSocket, &(ii+i)->bdaddr, sizeof(deviceName), deviceName, 0) < 0)
         {
-        	strcpy(name, "[unknown]");
+        	strcpy(deviceName, "[unknown]");
         }
-        printf("%s  %s\n", addr, name);
-        tempPair = std::make_pair<std::string, std::string>(addr, name);
+        
+        tempPair = std::make_pair<std::string, std::string>(deviceAddress, deviceName);
         devices.push_back(tempPair);
 
     }
