49e19030ee1e369a83248de64eca989a2d5730c0
Kyle Gonzalez <kylegonzalez2255@gmail.com>
2018-03-08 16:39:45 +1100

    Added windows build script.

---
+++ windowsCompile.bat
@@ -0,0 +1,6 @@
+# If you are having compilation errors which does not generate an ouput file:
+# Go to MinGW dir and rename mingw32 to mingw32old
+
+g++ test.cpp Socket/Socket.cpp ServerSocket/ServerSocket.cpp -std=c++11 -lws2_32 -o WindowsTest
+
+pause
