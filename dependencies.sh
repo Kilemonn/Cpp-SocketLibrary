cc7a3438bc8e4d934d048834a9e95c0bd2d662c4
KyleGonzalez <kylegonzalez2255@gmail.com>
2019-05-20 23:08:41 +1000

    Changes for docker example

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,9 @@
 
 # Install bluetooth library
-apt-get install libbluetooth-dev
+apt-get install -y libbluetooth-dev
 
 # Include glib-2.0
-apt-get install libglib2.0-dev
+apt-get install -y libglib2.0-dev
 
 # Install for hciconfig
-apt-get install bluez
+apt-get install -y bluez
