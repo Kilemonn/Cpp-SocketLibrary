6823f21ab45534892d451024deaf16f990cc6e5f
KyleGonzalez <kylegonzalez2255@gmail.com>
2020-07-23 21:41:37 +1000

    Remove windows compilation script.

--- windowsCompile.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: If you are having compilation errors which does not generate an ouput file:
-:: Go to MinGW dir and rename mingw32 to mingw32old
-
-g++ -Wall UnitTests/test.cpp Socket/Socket.cpp ServerSocket/ServerSocket.cpp -std=c++11 -lws2_32 -Llibs/ -lbthprops -liphlpapi -lBluetoothApis -o WindowsTest
-
-pause
