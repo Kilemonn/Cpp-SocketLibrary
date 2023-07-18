5fc9b38df293b0d962e885b91aab0b7496d8e54b
Kilemonn <kylegonzalez2255@gmail.com>
2023-07-18 19:56:35 +1000

    Update location to googletests lib.

---
+++ googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/hot_m/Documents/Repos/Cpp-SocketLibrary/libs/googletest-1.13.0/googletest
+# Build directory: /mnt/c/Users/hot_m/Documents/Repos/Cpp-SocketLibrary/libs/googletest-1.13.0/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
